796d88c346edb3726e45beb5dd189b4882b7e53d
Utkarsh Shrivastav <utkarsh.sri90@gmail.com>
2020-03-09 22:55:26 +0530

    multi tenant app complete

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+    after_create :create_tenant
+
+    private
+    def create_tenant
+        Apartment::Tenant.create(subdomain)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
+class SubdomainConstraint
+  def self.matches?(request)
+    subdomains = %w{www admin}
+    request.subdomain.present? && !subdomains.include?(request.subdomain)
+  end
+end 
+
 Rails.application.routes.draw do
+
+  constraints SubdomainConstraint do
+    resources :projects
+  end
+  resources :users
+
+  root 'users#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
